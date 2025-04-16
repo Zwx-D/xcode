091914f000331d8b9ec0c16d0240a1caf0bbb94a
ZwxMy <1306060912@qq.com>
2025-04-16 14:11:16 +0800

    fix 增加小程序轮播图接口

--- src/main/java/com/xcode/app/web/rest/WechatResource.java
+++ src/main/java/com/xcode/app/web/rest/WechatResource.java
@@ -2,14 +2,18 @@ package com.xcode.app.web.rest;
 
 import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
 import cn.binarywang.wx.miniapp.bean.WxMaPhoneNumberInfo;
+import com.xcode.app.service.CarouselImageService;
 import com.xcode.app.service.WechatService;
 import com.xcode.app.web.rest.api.WechatApi;
+import com.xcode.app.web.rest.vm.CarouselImageVM;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -19,6 +23,9 @@ public class WechatResource implements WechatApi {
     @Autowired
     private WechatService wechatService;
 
+    @Autowired
+    private CarouselImageService carouselImageService;
+
     @Override
     public ResponseEntity<WxMaJscode2SessionResult> code2Session(String appId, String code) {
         return ResponseEntity.ok(wechatService.code2Session(appId, code));
@@ -27,5 +34,10 @@ public class WechatResource implements WechatApi {
     @Override
     public ResponseEntity<WxMaPhoneNumberInfo> getuserphoneummber(String appId, String code) throws WxErrorException {
         return ResponseEntity.ok(wechatService.getuserphoneummber(appId, code));
+    }
+
+    @Override
+    public ResponseEntity<List<CarouselImageVM>> findAllCarouselImages() {
+        return ResponseEntity.ok(carouselImageService.findAll());
     }
 }
--- src/main/java/com/xcode/app/web/rest/api/WechatApi.java
+++ src/main/java/com/xcode/app/web/rest/api/WechatApi.java
@@ -2,17 +2,23 @@ package com.xcode.app.web.rest.api;
 
 import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
 import cn.binarywang.wx.miniapp.bean.WxMaPhoneNumberInfo;
+import com.xcode.app.web.rest.vm.CarouselImageVM;
 import me.chanjar.weixin.common.error.WxErrorException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 public interface WechatApi {
 
-    @GetMapping("/wechat/public/code2Session/{appId}/{code}")
-    ResponseEntity<WxMaJscode2SessionResult> code2Session(@PathVariable String appId, String code);
+    @GetMapping("/weChat/public/code2Session/{appId}/{code}")
+    ResponseEntity<WxMaJscode2SessionResult> code2Session(@PathVariable("appId") String appId, @PathVariable("code") String code);
 
-    @GetMapping("/wechat/public/getuserphoneummber/{appId}/{code}")
-    ResponseEntity<WxMaPhoneNumberInfo> getuserphoneummber(String appId, String code) throws WxErrorException;
+    @GetMapping("/weChat/public/getUserPhoneNumber/{appId}/{code}")
+    ResponseEntity<WxMaPhoneNumberInfo> getuserphoneummber(@PathVariable("appId") String appId, @PathVariable("code") String code) throws WxErrorException;
+
+    @GetMapping("/weChat/public/findAllCarouselImages")
+    ResponseEntity<List<CarouselImageVM>> findAllCarouselImages();
 
 }
